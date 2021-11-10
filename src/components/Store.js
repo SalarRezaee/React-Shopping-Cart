5222e7d918e6d96c8e9c4050ab9dd08d0d607b45
unknown <qsalar905@gmail.com>
2021-11-10 08:54:06 -0800

    adding some styles

--- src/components/Store.js
+++ src/components/Store.js
@@ -6,7 +6,7 @@ const Store = () => {
     const products = useContext(ProductsContext)
 
     return (
-        <div className="grid lg:grid-cols-4 md:gap-4 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 lg:gap-6 lg:auto-cols-max md:auto-cols-max sm:auto-cols-max">
+        <div className="grid lg:grid-cols-4 md:gap-4 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 lg:gap-6 lg:auto-cols-max md:auto-cols-max sm:auto-cols-max gap-y-6">
             {
                 products.map(product => <Product 
                     key={product.id} 
--- src/shared/Product.js
+++ src/shared/Product.js
@@ -11,24 +11,29 @@ const Product = ({ productData }) => {
         <div className="bg-white rounded-xl	 filter drop-shadow-2xl	">
             <img src={productData.image} alt="" style={{ width: '200px' , height : '200px' }} className="mx-auto m-5"/>
             <h1 className="text-center text-lg text-bold">{shorten(productData.title)}</h1>
-            <div className="flex px-5 my-5 justify-between">
-                <p className="bg-red-600 p-2 text-sm text-white text-opacity-90 rounded">{productData.price} $</p>
+            <div className="flex px-5 my-5 items-center justify-between">
+                <span className="bg-purple-400 text-purple-700 p-2 text-sm rounded"><Link to={`/products/${productData.id}`}>Details</Link></span>
+                <p className="p-2 text-lg">{productData.price} $</p>
             </div>
 
-            <div className="flex justify-between p-5">
-                    <span className="bg-purple-400 p-2 text-white rounded"><Link to={`/products/${productData.id}`}>Datiles</Link></span>
+            <div className="p-5 flex align-center justify-between">
+                  
                     {
                         isInCard(state, productData.id) ?
-                            <button className="bg-green-600 text-sm p-2 text-white rounded" onClick={() => dispatch({ type: "INCREASE", payload: productData })}>+</button> :
-                            <button className="bg-purple-600 text-sm p-2 text-white rounded" onClick={() => dispatch({ type: "ADD_ITEM", payload: productData })}>Add To Cart</button>
+                            <button className="bg-green-600 text-sm p-2 text-white rounded" onClick={() => dispatch({ type: "INCREASE", payload: productData })}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 22 22" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"/></svg>
+                            </button> :
+                            <button className="w-full p-2 shadow-xl text-sm rounded bg-purple-700 text-white" onClick={() => dispatch({ type: "ADD_ITEM", payload: productData })}>Add To Cart</button>
                     }
 
                     {
-                        quantitiyCount(state, productData.id) > 1 && <button className="bg-red-600 text-sm p-2 text-white rounded"  onClick={() => dispatch({ type: "DECREASE", payload: productData })}>-</button>
+                        quantitiyCount(state, productData.id) > 1 && <button className="bg-red-600 text-sm h-8 w-8 text-white rounded"  onClick={() => dispatch({ type: "DECREASE", payload: productData })}>-</button>
                     }
 
                     {
-                        quantitiyCount(state, productData.id) === 1 && <button className="bg-red-600 text-sm p-2 text-white rounded"  onClick={() => dispatch({ type: "REMOVE_ITEM", payload: productData })}>Remvoe Item</button>
+                        quantitiyCount(state, productData.id) === 1 && <button className="bg-red-600 text-sm p-2 text-white rounded"  onClick={() => dispatch({ type: "REMOVE_ITEM", payload: productData })}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 22 22" width="18" height="18"><path fill="none" d="M0 0h24v24H0z"/><path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z"/></svg>
+                        </button>
                     }
             </div>
 
