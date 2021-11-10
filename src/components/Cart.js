5dc7606db731f2a2f9b22c4d6324b02fcab26091
unknown <qsalar905@gmail.com>
2021-11-10 09:58:51 -0800

    Styling to the Details

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,28 +1,28 @@
 import { useContext } from "react"
-import { CardContext }  from "../context/ProductContextProvider/CartContextProvider"
+import { CardContext } from "../context/ProductContextProvider/CartContextProvider"
 import { shorten } from "../helper/function"
 const Cart = (props) => {
 
     let { dispatch } = useContext(CardContext)
 
-    const {image , title , price , quantitiy } = props.data
+    const { image, title, price, quantitiy } = props.data
 
 
     return (
-        <div>
-            <img src={image}/>
+        <div className="w-100 bg-white shadow-xl flex items-center my-2 p-5">
             <div>
-                <h1>{shorten(title)}</h1>
+            <img className="object-contain w-32 h-32" src={image} />
+                <h1 className="text-lg my-2">{shorten(title)}</h1>
                 <p>{price} $</p>
                 <span>{quantitiy}</span>
             </div>
-            <div>
+            <div className="flex">
                 {
                     quantitiy > 1 ?
-                    <button onClick={() => dispatch({type : "DECREASE" , payload :  props.data})}>-</button> : 
-                    <button onClick={() => dispatch({type : "REMOVE_ITEM" , payload :  props.data})}>delete</button>
+                        <button onClick={() => dispatch({ type: "DECREASE", payload: props.data })}>-</button> :
+                        <button onClick={() => dispatch({ type: "REMOVE_ITEM", payload: props.data })}>delete</button>
                 }
-                 <button onClick={() => dispatch({tyoe : "INCREASE" , payload : props.data})}>+</button>
+                <button onClick={() => dispatch({ tyoe: "INCREASE", payload: props.data })}>+</button>
             </div>
         </div>
     )
--- src/components/Productdetails.js
+++ src/components/Productdetails.js
@@ -7,17 +7,21 @@ const Productdetails = (props) => {
     const id = useParams("id").id
     const data = useContext(ProductsContext)
     const product = data[id - 1]
-    const {image , title , description , category , price} = product || {};
+    const { image, title, description, category, price } = product || {};
 
     return (
-        <div>
-            <img src={image} alt="product" />
+        <div className="flex xs-responsive items-center justify-between lg:flex-row xl:flex-row md:flex-col sm:flex-col xs:flex-col  space-x-6 mt-5 bg-white shadow-2xl p-5 rounded">
             <div>
-                <h1>{title}</h1>
-                <p>{description}</p>
-                <p>category : {category}</p>
-                <p>price : {price} $</p>
-                <Link to="/products">Back to Shop</Link>
+                <img src={image} height="200" alt="product" />
+            </div>
+            <div>
+                <h1 className="my-4 text-bold text-xl">{title}</h1>
+                <p className="text-justify my-5">{description}</p>
+                <div className="flex space-x-6 mb-4">
+                    <p>Category : {category}</p>
+                    <p>Price : {price} $</p>
+                </div>
+                <p className="bg-purple-500 my-2  text-white text-center p-2 rounded"><Link to="/products">Back to Shop</Link></p>
             </div>
         </div>
     )
