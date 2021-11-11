7c1c6fb045f07da2cac93b45ef703320cff50193
unknown <qsalar905@gmail.com>
2021-11-11 08:05:44 -0800

    Fix checkout & clear style

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -9,20 +9,30 @@ const Cart = (props) => {
 
 
     return (
-        <div className="w-100 bg-white shadow-xl flex items-center my-2 p-5">
-            <div>
-            <img className="object-contain w-32 h-32" src={image} />
-                <h1 className="text-lg my-2">{shorten(title)}</h1>
-                <p>{price} $</p>
-                <span>{quantitiy}</span>
-            </div>
-            <div className="flex">
-                {
-                    quantitiy > 1 ?
-                        <button onClick={() => dispatch({ type: "DECREASE", payload: props.data })}>-</button> :
-                        <button onClick={() => dispatch({ type: "REMOVE_ITEM", payload: props.data })}>delete</button>
-                }
-                <button onClick={() => dispatch({ tyoe: "INCREASE", payload: props.data })}>+</button>
+        <div className="w-full">
+            <div class="w-full">
+                <div class="flex my-3 items-center p-4 bg-white border-2 border-gray-200 rounded-lg shadow-sm dark:bg-gray-800">
+                    <img alt="mountain" class="w-20  rounded-md " src={image} />
+                    <div id="body" class="flex flex-col ml-4">
+                        <h4 id="name" class="text-xl font-semibold">{shorten(title)}</h4>
+                        <div class="flex mt-5">
+                            <p >quantitiy : {quantitiy}</p>
+                        </div>
+                        <p>Price : {price} $</p>
+                    </div>
+                    <div className="flex ml-auto flex-col">
+                        {
+                            quantitiy > 1 ?
+                            <button className="bg-red-600 text-sm h-8 w-8 text-white rounded"  onClick={() => dispatch({ type: "DECREASE", payload: props.data })}>-</button> :
+                                <button className="bg-red-600 text-sm p-2 text-white rounded" onClick={() => dispatch({ type: "REMOVE_ITEM", payload: props.data })}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 22 22" width="18" height="18"><path fill="none" d="M0 0h24v24H0z" /><path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z" /></svg>
+                                </button>
+                        }
+                        <button className="bg-green-600 text-sm p-2 text-white my-2 rounded" onClick={() => dispatch({ type: "INCREASE", payload: props.data })}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 22 22" width="18" height="18"><path fill="none" d="M0 0h24v24H0z" /><path d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z" /></svg>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -1,43 +1,54 @@
 import { useContext } from "react"
-import { CardContext }  from "../context/ProductContextProvider/CartContextProvider"
+import { CardContext } from "../context/ProductContextProvider/CartContextProvider"
 import { Link } from "react-router-dom"
 import Cart from "./Cart"
 
+import success from "../assets/success.png"
+
+
+
 const ShopCart = () => {
 
-    let {state ,  dispatch} = useContext(CardContext)
+    let { state, dispatch } = useContext(CardContext)
 
     return (
         <>
-            <div>
-                 {
-                     state.selectedItem.map(item => <Cart key={item.id} data={item} />)
-                 }   
+            <div dir="rtl" >
+                <div className="grid gap-1 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 xs:grid-cols-1">
+                    {
+                        state.itemCounter > 0 && <div dir="ltr" className="ml-auto mt-2 rounded border-2 lg:w-3/4 md:w-full sm:w-full xs:w-full h-52 p-5">
+                            <h1 className="text-2xl mb-3 rounded">Shopping Cart</h1>
+                            <p className="py-2"><span>Total Counter : {state.itemCounter}</span></p>
+                            <p className="py-2"><span>Total Price : {state.total} $</span></p>
+                            <div className="flex justify-between my-2">
+                                <button className="p-2 shadow-xl text-sm rounded bg-red-700 text-white" onClick={() => { dispatch({ type: "CLEAR" }) }}>Clear</button>
+                                <button className="p-2 shadow-xl text-sm rounded bg-green-700 text-white" onClick={() => { dispatch({ type: "CHECKOUT" }) }}>Checkout</button>
+                            </div>
+                        </div>
+                    }
 
-                 {
-                     state.itemCounter > 0 && <div>
-                         <p><span>Total Counter : {state.itemCounter}</span></p>
-                         <p><span>Total Price : {state.total} $</span></p>
+                    {
+                        state.selectedItem && <div className="w-full" dir="ltr">
+                            {state.selectedItem.map(item => <Cart key={item.id} data={item} />)}
+                        </div>
+                    }
+                </div>
 
-                         <button onClick={() => {dispatch({type : "CLEAR" })}}>Clear</button>
-                         <button onClick={() => {dispatch({type : "CHECKOUT"})}}>Checkout</button>
-                     </div>
-                 }
-
-                 {
-                     state.checkout && <div>
-                         CheckOut Confirm
-                         <Link to="/products">But More</Link>
-                     </div>
-                 }
-
-                 {
-                        !state.checkout && state.itemCounter === 0 && <div>
-                        You Want to Buy?
-                        <Link to="/products">Go To the Shop</Link>
+                {
+                    state.checkout && <div dir="ltr" className="w-full mx-auto flex flex-col items-center space-y-5" >
+                        <img src={success} className="my-4 mx-1" />
+                        <p className="my-5">Your payment was successful</p>
+                        <button className="p-2 shadow-xl text-sm rounded bg-green-700 text-white"><Link to="/products">Buy More</Link></button>
                     </div>
-                 }
-                 
+                }
+
+                {
+                    !state.checkout && state.itemCounter === 0 && <div dir="ltr" className="w-full mx-auto flex flex-col items-center space-y-5" >
+                        <p className="my-5">Your Shopping Cart is Empty</p>
+                        <button className="p-2 shadow-xl text-sm rounded bg-green-700 text-white"><Link to="/products">Buy More</Link></button>
+                    </div>
+                }
+
 
             </div>
         </>
